00e78cc3247b6758563e1cd60d8d11c2777501f8
Mateus Barbosa <pg9779628@gmail.com>
2024-01-30 21:30:10 -0300

    First Commit!

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+
+const path = require('path');
+const app = express();
+const prisma = new PrismaClient();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, './views'));
+
+app.get('/', async (req,res) => {
+    await prisma.days.create();
+    const days = await prisma.days.findMany();
+    res.render('index', {days: days});
+});
+
+app.listen(3000,()=>{console.log('server running on port 3000!')});
